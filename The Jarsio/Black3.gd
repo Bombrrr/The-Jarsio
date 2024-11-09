e50e57f6889ace558885167f8f349b447ef08633
Bombrrr <arealhbombplayz@gmail.com>
2024-11-09 22:16:38 +1100

    amazeballs

--- The Jarsio/Black3.gd
+++ The Jarsio/Black3.gd
@@ -6,6 +6,7 @@ extends Sprite2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	fad = true
+	self.show()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 @warning_ignore("unused_parameter")
