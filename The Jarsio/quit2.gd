de6087c6af2de1f647d83ecac7e993d246e12307
Bombrrr <arealhbombplayz@gmail.com>
2024-11-10 12:09:02 +1100

    theming

--- The Jarsio/quit2.gd
+++ The Jarsio/quit2.gd
@@ -2,7 +2,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$VBoxContainer/No.grab_focus()
+	$VBoxContainer/VBoxContainer/No.grab_focus()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 @warning_ignore("unused_parameter")
 func _process(delta):
